81534787b8182f7491ab5066ef7aa5502f704517
Hanno Schlichting <hanno@hannosch.eu>
2008-10-15 12:20:46 +0000

    Update after 0.16
    svn path=/buildout/plone.recipe.zope2zeoserver/trunk/; revision=73686

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2zeoserver"
-version = '0.16'
+version = '0.17'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
