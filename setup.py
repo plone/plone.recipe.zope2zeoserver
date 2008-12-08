8646e06c41d65ebabb3d6abe057363ab18661a5a
Sidnei da Silva <sidnei.da.silva@gmail.com>
2008-12-08 14:37:15 +0000

    - Remove dev marker
    svn path=/buildout/plone.recipe.zope2zeoserver/tags/1.1/; revision=77061

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2zeoserver"
-version = '1.0dev'
+version = '1.1'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
