f9414edb7aeee0dd4cca7287231fd0a04780c75f
Patrick Gerken <do3cc@patrick-gerken.de>
2013-01-29 12:41:21 +0100

    Preparing release 1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2zeoserver"
-version = '2.0'
+version = '1.5'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
