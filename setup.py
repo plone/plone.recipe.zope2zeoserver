81de5551fce10662cec94cf9e2b5b1c800dd9699
Patrick Gerken <do3cc@patrick-gerken.de>
2013-01-29 12:43:33 +0100

    Back to development: 1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 name = "plone.recipe.zope2zeoserver"
-version = '1.5'
+version = '1.6.dev0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
